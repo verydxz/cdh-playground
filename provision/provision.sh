235302b5a7a453edab846c09e68214af6892db04
verydxz <verydxz@hotmail.com>
2017-07-26 08:46:37 +0800

    add default user to sudo group

--- provision/provision.sh
+++ provision/provision.sh
@@ -20,6 +20,19 @@ sudo chown $_USER:$_GROUP /home/$_USER/.ssh/authorized_keys
 sudo chmod 400 /home/$_USER/.ssh/id_rsa
 echo "[provision] customized SSH key installed."
 
+# hosts
+if [ -e /tmp/hosts_updated ]; then
+  echo "[provision] hosts file already updated."
+else
+  sudo sed -i '$a \
+192.168.99.100 cdh0 \
+192.168.99.101 cdh1 \
+192.168.99.102 cdh2 \
+192.168.99.103 cdh3' /etc/hosts
+  echo "[provision] hosts file updated."
+  sudo touch /tmp/hosts_updated
+fi
+
 # apt
 if [ -e /etc/apt/sources.list ]; then
   sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
@@ -27,15 +40,6 @@ fi
 sudo cp /tmp/sources.list /etc/apt/sources.list
 sudo apt update
 
-# hosts
-if [ -e /tmp/hosts_updated ]; then
-  echo "[provision] hosts file already updated"
-else
-  sudo sed -i '$a \
-192.168.99.100 cdh0 \
-192.168.99.101 cdh1 \
-192.168.99.102 cdh2 \
-192.168.99.103 cdh3' /etc/hosts
-  echo "[provision] hosts file updated"
-  sudo touch /tmp/hosts_updated
-fi
+# root privilege
+sudo usermod -G sudo $_USER
+echo "[provision] user $_USER added to sudo group."
